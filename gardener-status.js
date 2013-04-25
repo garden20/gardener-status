12e58aeaab41c899c630d9beb7292c0af384f11d
Ryan Ramage <ryan.ramage@gmail.com>
2013-04-25 16:29:02 -0600

    Initial commit

---
+++ gardener-status.js
@@ -0,0 +1,86 @@
+/**
+ * Universal module definition
+ */
+
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        module.exports = factory(require('ractive-couch'), require('pico-couch-ddoc'));
+    } else if (typeof define === 'function' && define.amd) {
+        define(['ractive-couch', 'pico-couch-ddoc'], factory);
+    } else {
+        root.GardenerStatus = factory(root.RactiveCouch, root.PicoCouchDDoc);
+    }
+}(this, function (RactiveCouch, pico_couch_ddoc) {
+
+
+    return GardenerStatus;
+
+
+    function GardenerStatus(dashboard_url, ddoc_path, elem_id) {
+
+        pico_couch_ddoc.view(dashboard_url,  'gardener',  getView(), function(err){
+            if (err) return showError(err, elem_id);
+
+            new RactiveCouch.View(dashboard_url, 'gardener/gardener', {
+                el : elem_id,
+                template: '{{#rows}} <div>{{key[1]}} {{value.percent}} {{value.msg}} </div>{{/rows}}',
+                include_docs: true,
+                view_options: {
+                    startkey: [ddoc_path],
+                    endkey: [ddoc_path, {}, {}],
+                    group: true,
+                    group_level: 2
+                },
+                is_same: is_same,
+                map_change: map_change
+            });
+        });
+    }
+
+    function showError(err, elem_id) {
+        console.log(err);
+    }
+
+    function map_change(change) {
+        return {
+            key: [change.doc.path, change.doc.module],
+            value: {
+                percent: change.doc.percent,
+                msg: change.doc.msg
+            }
+        };
+    }
+
+    function is_same(row, change) {
+        if (row.key[0] === change.doc.path && row.key[1] === change.doc.module) return true;
+        return false;
+    }
+
+
+    function getView() {
+        return {
+            map: function(doc) {
+                if (doc.type !== 'gardener_progress') return;
+                emit([doc.path, doc.module, doc.time], {percent: doc.percent, msg: doc.msg, time: doc.time});
+            },
+            reduce: function(keys, values) {
+                var max;
+
+                for (var i=0; i < values.length; i++) {
+                    var val = values[i];
+                    if (!max) max = val;
+                    else {
+                        if (val.time > max.time) max = val;
+                    }
+                }
+                return max;
+            }
+        };
+    }
+
+
+
+
+
+// end of UMD
+}));
---
+++ jam/require.config.js
@@ -0,0 +1,47 @@
+var jam = {
+    "packages": [
+        {
+            "name": "couchr",
+            "location": "jam/couchr",
+            "main": "couchr-browser.js"
+        },
+        {
+            "name": "events",
+            "location": "jam/events",
+            "main": "events.js"
+        },
+        {
+            "name": "jquery",
+            "location": "jam/jquery",
+            "main": "dist/jquery.js"
+        },
+        {
+            "name": "pico-couch-ddoc",
+            "location": "jam/pico-couch-ddoc",
+            "main": "pico-couch-ddoc.js"
+        },
+        {
+            "name": "ractive",
+            "location": "jam/ractive",
+            "main": "Ractive.js"
+        },
+        {
+            "name": "ractive-couch",
+            "location": "jam/ractive-couch",
+            "main": "ractive-couch.js"
+        }
+    ],
+    "version": "0.2.11",
+    "shim": {}
+};
+
+if (typeof require !== "undefined" && require.config) {
+    require.config({packages: jam.packages, shim: jam.shim});
+}
+else {
+    var require = {packages: jam.packages, shim: jam.shim};
+}
+
+if (typeof exports !== "undefined" && typeof module !== "undefined") {
+    module.exports = jam;
+}
